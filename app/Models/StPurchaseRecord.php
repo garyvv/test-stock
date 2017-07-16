1c28cf380ca5eee4d617fd3f90be51da6a328863
lvsongdong <lvsongdong@maka.im>
2017-07-16 21:49:51 +0800

    modify:rapyd filter

--- app/Models/StPurchaseRecord.php
+++ app/Models/StPurchaseRecord.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 
 class StPurchaseRecord extends Model
 {
@@ -15,4 +15,13 @@ class StPurchaseRecord extends Model
         return $this->hasOne('App\Models\StCategory', 'category_id', 'category_id');
     }
 
+    public static function rapydAllCategories()
+    {
+        return DB::table('st_purchase_records AS p')
+            ->leftJoin('st_categories AS c', 'p.category_id', '=', 'c.category_id')
+            ->select(
+                DB::raw('distinct p.category_id'),
+                'c.name'
+            );
+    }
 }
--- resources/views/admin-custom/base.blade.php
+++ resources/views/admin-custom/base.blade.php
@@ -1,14 +1,18 @@
 <style>
-    .info-box-content{
+    .info-box{
         cursor: pointer;
+    }
+    .info-box:hover{
+        background-color: #f5f5f5;
+        width: 95%;
     }
 </style>
 <div class="row">
     <div class="col-md-3 col-sm-6 col-xs-12">
-        <div class="info-box">
+        <div class="info-box" onclick="location='/admin/customers'">
             <span class="info-box-icon bg-aqua"><i class="fa fa-users"></i></span>
 
-            <div class="info-box-content" onclick="location='/admin/customers'">
+            <div class="info-box-content">
                 <span class="info-box-text">用户数量</span>
                 <span class="info-box-number">{{$data['user_count']}}</span>
             </div>
@@ -18,10 +22,10 @@
     </div>
     <!-- /.col -->
     <div class="col-md-3 col-sm-6 col-xs-12">
-        <div class="info-box">
+        <div class="info-box" onclick="location='/admin/orders'">
             <span class="info-box-icon bg-green"><i class="fa fa-shopping-cart"></i></span>
 
-            <div class="info-box-content" onclick="location='/admin/orders'">
+            <div class="info-box-content">
                 <span class="info-box-text">订单数量</span>
                 <span class="info-box-number">{{$data['order_count']}}</span>
             </div>
@@ -31,10 +35,10 @@
     </div>
     <!-- /.col -->
     <div class="col-md-3 col-sm-6 col-xs-12">
-        <div class="info-box">
+        <div class="info-box" onclick="location='/admin/sellers'">
             <span class="info-box-icon bg-yellow"><i class="fa fa-flag-o"></i></span>
 
-            <div class="info-box-content" onclick="location='/admin/sellers'">
+            <div class="info-box-content">
                 <span class="info-box-text">商家数量</span>
                 <span class="info-box-number">{{$data['seller_count']}}</span>
             </div>
@@ -44,10 +48,10 @@
     </div>
     <!-- /.col -->
     <div class="col-md-3 col-sm-6 col-xs-12">
-        <div class="info-box">
+        <div class="info-box" onclick="location='/admin/categories'">
             <span class="info-box-icon bg-red"><i class="fa fa-files-o"></i></span>
 
-            <div class="info-box-content" onclick="location='/admin/categories'">
+            <div class="info-box-content">
                 <span class="info-box-text">商品数量</span>
                 <span class="info-box-number">{{$data['product_count']}}</span>
             </div>
