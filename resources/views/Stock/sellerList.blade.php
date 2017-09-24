0ffcae50d48294db5eeb040e3e698d1a37555cde
370977448@qq.com <gary@Gary-MacBook-Pro.local>
2017-09-25 02:25:22 +0800

    search demo

--- resources/views/Stock/sellerList.blade.php
+++ resources/views/Stock/sellerList.blade.php
@@ -7,6 +7,22 @@
     </style>
     <a href="/sellers/create" class='nav-btn' >CREATE</a>
     <p class="p-title" onclick="javascript:window.location.href='/'"><i class="weui-icon-circle"></i>首页</p>
+
+    <div class="weui-search-bar" id="searchBar">
+        <form class="weui-search-bar__form" id="search">
+            <div class="weui-search-bar__box">
+                <i class="weui-icon-search"></i>
+                <input type="search" class="weui-search-bar__input" id="searchInput" placeholder="搜索" required="">
+                <a href="javascript:" class="weui-icon-clear" id="searchClear"></a>
+            </div>
+            <label class="weui-search-bar__label" id="searchText">
+                <i class="weui-icon-search"></i>
+                <span>搜索</span>
+            </label>
+        </form>
+        <a href="javascript:" class="weui-search-bar__cancel-btn" id="searchCancel">取消</a>
+    </div>
+
     <div id="sellerLists">
         {{--//数据加载在此--}}
     </div>
