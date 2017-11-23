b2a902c33b9db77561c6c4806e6eb7d1a7b7f682
lvsongdong <lvsongdong@maka.im>
2017-11-23 17:07:23 +0800

    ueditor

--- resources/views/html.blade.php
+++ resources/views/html.blade.php
@@ -2,13 +2,17 @@
 @section('content')
     {!! Rapyd::head() !!}
 
-    <link rel="stylesheet" href="{{ asset("/js/simditor/styles/simditor.css") }}">
+    {{--<link rel="stylesheet" href="{{ asset("/js/simditor/styles/simditor.css") }}">--}}
 
-    <script type="text/javascript" src="{{ asset ("/js/simditor/scripts/jquery.min.js") }}"></script>
-    <script type="text/javascript" src="{{ asset ("/js/simditor/scripts/module.js") }}"></script>
-    <script type="text/javascript" src="{{ asset ("/js/simditor/scripts/hotkeys.js") }}"></script>
-    <script type="text/javascript" src="{{ asset ("/js/simditor/scripts/uploader.js") }}"></script>
-    <script type="text/javascript" src="{{ asset ("/js/simditor/scripts/simditor.js") }}"></script>
+    {{--<script type="text/javascript" src="{{ asset ("/js/simditor/scripts/jquery.min.js") }}"></script>--}}
+    {{--<script type="text/javascript" src="{{ asset ("/js/simditor/scripts/module.js") }}"></script>--}}
+    {{--<script type="text/javascript" src="{{ asset ("/js/simditor/scripts/hotkeys.js") }}"></script>--}}
+    {{--<script type="text/javascript" src="{{ asset ("/js/simditor/scripts/uploader.js") }}"></script>--}}
+    {{--<script type="text/javascript" src="{{ asset ("/js/simditor/scripts/simditor.js") }}"></script>--}}
+
+    {{--百度 UEditor--}}
+    <script type="text/javascript" src="{{ asset ("/js/ueditor/ueditor.config.js") }}"></script>
+    <script type="text/javascript" src="{{ asset ("/js/ueditor/ueditor.all.min.js") }}"></script>
 
     <div style="padding:2%">
         <div class="rpd-edit">
@@ -17,7 +21,8 @@
             <br>
             <form id="edit-content" action="/admin/html" method="POST">
                 <label for="redactor_content">内容</label>
-                <textarea id="redactor_content" name="content" style="height: 560px;">{!! $content !!}</textarea>
+                {{--<textarea id="redactor_content" name="content" style="height: 560px;">{!! $content !!}</textarea>--}}
+                <script id="redactor_content" name="content" type="text/plain"></script>
                 <input hidden="hidden" name="id" value="{!! $id !!}" />
                 <input hidden="hidden" name="type" value="{!! $type !!}" />
                 <input hidden="hidden" name="image_dir" value="{!! $imageDir !!}" />
@@ -31,9 +36,15 @@
 
     <script>
 //        $('#redactor_content').redactor();
-        var editor = new Simditor({
-            textarea: $('#redactor_content')
-            //optional options
+//        var editor = new Simditor({
+//            textarea: $('#redactor_content')
+//            //optional options
+//        });
+
+        var ue = UE.getEditor("redactor_content");
+        ue.ready(function() {
+            //设置编辑器的内容
+            ue.setContent('{!! $content !!}');
         });
 //        loading
         function loading() {
